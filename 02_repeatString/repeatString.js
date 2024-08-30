5fe8f5b26fc0011b508028b2886e99fe207d64ea
Rohan Yadav <rohan151025@gmail.com>
2024-08-30 08:34:27 +0530

    Solve exercise-02 'repeatString'

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(text, times) {
+    let repeat = times;
+    let result = '';
+    while (repeat > 0) {
+        result += text;
+        repeat--;
+    }
+    if (times < 0) return 'ERROR';
+    return result;
 };
+
+// console.log(repeatString('bye', 0));
 
 // Do not edit below this line
 module.exports = repeatString;
