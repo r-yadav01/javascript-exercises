0be84bfedca87273d0a033496add198f931e4f7a
Rohan Yadav <rohan151025@gmail.com>
2024-08-30 08:51:20 +0530

    Solve exercise-03 'reverse a string'

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let reverseString = '';
+    const textArray = [];
+    for (let letter of text) {
+        textArray.push(letter);
+    }
 
+    for (let i = textArray.length-1; i >= 0; i--) {
+        reverseString += textArray[i];
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
