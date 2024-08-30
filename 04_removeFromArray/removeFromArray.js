e605ff92d0790592f11024de2c5540ce65135e13
Rohan Yadav <rohan151025@gmail.com>
2024-08-30 09:34:17 +0530

    Solve exercise-04 'removeFromArray'

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let arr = args.shift();
+
+    for (let i = 0; i < args.length; i++) {
+        for (let j = 0; j < arr.length;) {
+            if (arr[j] === args[i]) {
+                arr.splice(j,1);
+            }
+            else {
+                ++j;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
