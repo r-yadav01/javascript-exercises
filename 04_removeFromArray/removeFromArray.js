41f14d0faa49958ea2e46d94166daf95bb537e48
Rohan Yadav <rohan151025@gmail.com>
2024-08-30 19:14:44 +0530

    Improve the approach to solve exercise-04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,24 @@
 const removeFromArray = function(...args) {
     let arr = args.shift();
+    let newArr = [];
 
-    for (let i = 0; i < args.length; i++) {
-        for (let j = 0; j < arr.length;) {
-            if (arr[j] === args[i]) {
-                arr.splice(j,1);
-            }
-            else {
-                ++j;
-            }
+    // for (let i = 0; i < args.length; i++) {
+    //     for (let j = 0; j < arr.length;) {
+    //         if (arr[j] === args[i]) {
+    //             arr.splice(j,1);
+    //         }
+    //         else {
+    //             ++j;
+    //         }
+    //     }
+    // }
+
+    arr.forEach(element => {
+        if (!args.includes(element)) {
+            newArr.push(element);
         }
-    }
-    return arr;
+    });
+    return newArr;
 };
 
 // Do not edit below this line
