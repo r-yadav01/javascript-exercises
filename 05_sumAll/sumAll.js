c40aff2ab701d7b7f976cacfdf6c65c0ef18a045
Rohan Yadav <rohan151025@gmail.com>
2024-08-30 19:29:21 +0530

    Improve the algorithm to solve exercise-05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,43 +1,39 @@
 const sumAll = function(fromInt, toInt) {
     let sum = 0;
+
     // if (typeof fromInt === 'number' && typeof toInt === 'number') {
-    //     if (fromInt % 10 === 0 && toInt % 10 === 0) {
-    //         
-    //         for (let i = fromInt; i <= toInt; i++) {
-    //             sum += i;
-    //         }
+    //     if (fromInt < 0 || toInt < 0) return 'ERROR';
+    //     else if (Math.floor(fromInt) !== fromInt || Math.floor(toInt) !== toInt) return 'ERROR';
+
+    //     if (fromInt > toInt) {
+    //         let temp = fromInt;
+    //         fromInt = toInt;
+    //         toInt = temp;
     //     }
-    //     else {
-    //         return 'ERROR';
+
+    //     for (let i = fromInt; i <= toInt; i++) {
+    //         sum += i;
     //     }
     // }
     // else {
     //     return 'ERROR';
     // }
 
-    if (typeof fromInt === 'number' && typeof toInt === 'number') {
-        if (fromInt < 0 || toInt < 0) return 'ERROR';
-        else if (Math.floor(fromInt) !== fromInt || Math.floor(toInt) !== toInt) return 'ERROR';
-
-        if (fromInt > toInt) {
-            let temp = fromInt;
-            fromInt = toInt;
-            toInt = temp;
-        }
-
-        for (let i = fromInt; i <= toInt; i++) {
-            sum += i;
-        }
-    }
-    else {
-        return 'ERROR';
+    if (!Number.isInteger(fromInt) || !Number.isInteger(toInt)) return "ERROR";
+    if (fromInt < 0 || toInt < 0) return "ERROR";
+    if (fromInt > toInt) {
+        let temp = fromInt;
+        fromInt = toInt;
+        toInt = temp;
     }
 
-    
+    for (let i = fromInt; i <= toInt; i++) {
+        sum += i;
+    }
     return sum;
 };
 
-console.log(sumAll(1,5));
+// console.log(sumAll(1,5));
 
 // why is this 'false' fromInt instanceof Number
 // but this is 'number' by typeof fromInt
