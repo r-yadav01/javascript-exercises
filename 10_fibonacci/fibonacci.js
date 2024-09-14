b49590af7f9d8463312113403d530d212017fd4b
Rohan Yadav <rohan151025@gmail.com>
2024-09-14 13:36:32 +0530

    Solve exercise-10 'fibonacci'

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    let sequence = [0,1];
+    if (index < 0) return "OOPS";
+    else if (index == 0) return sequence[0];
+    for (let i = 0; i < index-1; i++) {
+        let next = sequence[i] + sequence[i+1];
+        sequence.push(next);
+    }
+    sequence.shift();
+    // console.log(sequence);
+    return sequence.pop();
 };
 
+console.log(fibonacci(4));
 // Do not edit below this line
 module.exports = fibonacci;
