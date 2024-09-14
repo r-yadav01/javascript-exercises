d84b1b18083b175340f2212fa0267d17a50e90a2
Rohan Yadav <rohan151025@gmail.com>
2024-09-14 15:40:14 +0530

    Complete javascript-exercises repository of TOP
    done with this now time to move on to the calculator project after that
    foundations will be completed.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(objArr) {
+    let oldestAge = objArr.reduce((acc, curr) => {
+        let currYear = new Date().getFullYear();
+
+        if (acc.age == undefined) {
+            if (acc.yearOfDeath == undefined) {
+                acc.age = currYear - acc.yearOfBirth;
+            }
+            else {
+                acc.age = acc.yearOfDeath - acc.yearOfBirth;
+            }
+        }
+        
+        if (curr.yearOfDeath !== undefined) {
+            curr.age = curr.yearOfDeath - curr.yearOfBirth;
+        }
+        else {
+            curr.age = currYear - curr.yearOfBirth;
+        }
+        if (curr.age > acc.age) 
+            return curr;
+        else return acc;
+    })
+    return oldestAge;
 
 };
 
