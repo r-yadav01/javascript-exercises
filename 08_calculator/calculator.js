49c842b6fed18a23c1c372fcdf9404d0f2a84b24
Rohan Yadav <rohan151025@gmail.com>
2024-09-14 08:10:51 +0530

    Solve exercise-08 'calculator'

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(var1, var2) {
+	return (var1 + var2);
 };
 
-const subtract = function() {
-	
+const subtract = function(var1, var2) {
+	return (var1 - var2);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = arr.reduce((acc, curr) => {
+    return (acc + curr)
+  }, 0)
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let prod = arr.reduce((acc, curr) => (acc * curr), 1);
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(var1, var2) {
+	return Math.pow(var1, var2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0) return 1;
+  let numArr = [...Array(num).keys()];
+  numArr.shift();
+  numArr.push(num);
+  return numArr.reduce((acc, curr) => acc *= curr, 1);
 };
 
 // Do not edit below this line
