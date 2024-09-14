8260381f2edab529076b2ced67c5b515d1824621
Rohan Yadav <rohan151025@gmail.com>
2024-09-14 13:49:51 +0530

    Solve exercise-11 'getTheTitles'

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(objArr) {
+    let titleArr = [];
+    objArr.forEach(obj => {
+        titleArr.push(obj["title"]);
+    });
+    return titleArr;
 };
 
 // Do not edit below this line
